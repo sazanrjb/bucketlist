cb7d8a69f15f05ebadfe08e0732cdcfce34ab053
Sajan Rajbhandari <sazanrjb@gmail.com>
2019-05-27 21:41:28 +0545

    Install vue and tailwindcss

--- app/wish/models/Wish.js
+++ app/wish/models/Wish.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
 const schema = mongoose.schema({
-    id: mongoose.Types.ObjectId,
     title: String,
-    description: String,
     date: { type: Date },
-    created_at: { type: Date, default: Date.now }
+    is_succeeded: { type: Number, default: 0 },
+    created_at: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model('Wish', schema);
